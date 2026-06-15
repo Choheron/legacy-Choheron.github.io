212d6905d773cc06a8b538a2de53bd3c2c0e3076
Thomas Campbell <trc527@gmail.com>
2026-06-15 10:22:19 -0500

    Update links and resume

--- choheron.github.io/src/components/Linkblock.js
+++ choheron.github.io/src/components/Linkblock.js
@@ -8,7 +8,7 @@ export default function LinkBlock() {
 				<div className="linkBlock">
 					<a href="https://www.linkedin.com/in/thomasrichardcampbell/" target="_blank" rel="noreferrer noopener" title="LinkedIn"><i className='fab fa-linkedin-in'></i></a>
 					<a href="https://github.com/Choheron" target="_blank" rel="noreferrer noopener" title="GitHub"><i className='fab fa-github'></i></a>
-					<a href="./data/Thomas_Campbell_Resume.pdf" target="_blank" rel="noreferrer noopener" title="Resume"><i className="fas fa-file"></i></a>
+					<a href="./data/Thomas_Campbell_Resume_June2026.pdf" target="_blank" rel="noreferrer noopener" title="Resume"><i className="fas fa-file"></i></a>
 					<a href="https://choheron.artstation.com/" target="_blank" rel="noreferrer noopener" title="ArtStation"><i className='fab fa-artstation'></i></a>
 					<a href="mailto:trc527@gmail.com" target="_blank" rel="noreferrer noopener" title="Email: trc527@gmail.com"><i className='fa fa-envelope'></i></a>
 				</div>
